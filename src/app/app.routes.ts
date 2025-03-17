860823967ed6614f45fe0e9b05da11dd974c1d6e
DavidHarrisonHaskell <david.h.haskell@gmail.com>
2025-03-17 13:28:35 +0200

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/pages-area/home/home.component';
 import { AboutComponent } from './components/pages-area/about/about.component';
 import { ProjectsComponent } from './components/pages-area/projects/projects.component';
 import { ContactComponent } from './components/pages-area/contact/contact.component';
+import { Page404Component } from './components/layout-area/page404/page404.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: HomeComponent },
     { path: "about", component: AboutComponent },
     { path: "projects", component: ProjectsComponent },
-    { path: "contact", component: ContactComponent }
+    { path: "contact", component: ContactComponent },
+    { path: "**", component: Page404Component }
 ];
