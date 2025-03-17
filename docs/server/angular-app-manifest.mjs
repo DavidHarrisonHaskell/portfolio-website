37374d3df127d12b4320a865c2ac4a65cc3126b1
DavidHarrisonHaskell <david.h.haskell@gmail.com>
2025-03-17 13:02:59 +0200

    Updated build for GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: './',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 699, hash: '6d43cda1ad713e9185ed41bac5bdd8363bef21283cf1bbde4e069041587ceb51', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1031, hash: 'd2d5eb5cdcf63f7225d1c1243412d85df46eb7a042fe24183a44ce78ff1ed334', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 700, hash: '9c9d518abb8f96d507cf55bc86600c9cdd111665345f433515f24adca60e775e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1032, hash: '074d1783e5bee82c0b42bb90b1020d208aed5e00d6e6871b2d1d17bd173b7b8c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-M2VJIZ63.css': {size: 56, hash: 'T/uAenF+huo', text: () => import('./assets-chunks/styles-M2VJIZ63_css.mjs').then(m => m.default)}
   },
 };
